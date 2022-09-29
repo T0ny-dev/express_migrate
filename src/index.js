d935fd740c3f4379056eb0d6f1193a84ed9caf95
Toni-dev <antonioraweb@gmail.com>
2022-09-29 13:53:36 -0500

    añadido los modelos de tablas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import app from './app.js'
 import {sequelize} from './database/database.js'
+import './models/project.js'
+import './models/task.js'
 
 async function main () {
   try {
-  await sequelize.authenticate();
-  console.log("Connection has been established successfully");
-  app.listen(3000);
-  console.log("servidor escuchando en el puerto 3000");
+    await sequelize.sync()
+    app.listen(3000);
+    console.log("servidor escuchando en el puerto 3000");
   } catch (error) {
     console.error("Unable to connect to the database", error);
   }
